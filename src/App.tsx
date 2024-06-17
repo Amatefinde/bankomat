a0661e71070c22c14bbf6b3e9e03dca5c92a39b1
Amatefinde <mynameiiiypuk@yandex.ru>
2024-06-17 21:27:08 +0300

    restrict money input for only numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import  {useState} from 'react'
+import {useState} from 'react'
 import "./App.css"
 import {Box, Button, Card, Input, Sheet, Slider, Typography} from "@mui/joy";
-import { setNumberCassette} from "./store/cassettes/cassettesSlice.ts";
+import {setNumberCassette} from "./store/cassettes/cassettesSlice.ts";
 import CassetteComponent from "./components/СassetteComponent.tsx";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "./store";
 import calculateWithdrawal from "./utils/calculateWithdrawal.ts";
+import isNumericString from "./utils/isNumericString.ts";
 
 function App() {
     const dispatch = useDispatch<AppDispatch>();
 
-    function handleChange(e: Event, ) {
+    function handleChange(e: Event,) {
         // @ts-ignore
         const newValue = e.target?.value
         dispatch(setNumberCassette(newValue))
@@ -20,22 +21,32 @@ function App() {
     const cassettes = useSelector((state: RootState) => state.cassettes.cassettes);
     const [userMoney, setUserMoney] = useState<number>(0)
     const [calculateFeedback, setCalculateFeedback] = useState("")
+
     function handleCalculate() {
         setCalculateFeedback(calculateWithdrawal(cassettes, userMoney))
     }
 
+    function handleUserMoneyInput(e: React.ChangeEvent<HTMLInputElement>) {
+        if (isNumericString(e.target.value)) {
+            if (e.target.value != "") {
+                setUserMoney(parseInt(e.target.value))
+            } else {
+                setUserMoney(e.target.value)
+            }
+        }
+    }
 
     return <Sheet sx={{width: 600, borderRadius: 20, padding: 2}}>
         <Typography level={"h1"}>ATM Calculator</Typography>
         <Box sx={{display: "flex", alignItems: "center"}}>
             <Typography width={400} level={"title-lg"}>Number cassette</Typography>
-        <Slider valueLabelDisplay="on" value={cassettes.length} onChange={handleChange} max={8} min={1}/>
+            <Slider valueLabelDisplay="on" value={cassettes.length} onChange={handleChange} max={8} min={1}/>
         </Box>
 
         <Card variant={"soft"} sx={{margin: 1}}>
             <Typography level={"title-md"}>Money requested (in rub):</Typography>
             {/*@ts-ignore*/}
-            <Input value={userMoney} onChange={e => setUserMoney(e.target.value)}/>
+            <Input value={userMoney} onChange={handleUserMoneyInput}/>
             {calculateFeedback}
             <Button onClick={handleCalculate}>Calculate</Button>
         </Card>
